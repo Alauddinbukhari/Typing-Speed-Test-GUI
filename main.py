1ee73d6c6ac0e916a5d425aa54a73ae8ce504d94
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2024-02-22 21:13:30 -0700

    mirror changes to functions

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from para_sentence import paragraph
 window= Tk()
 window.config(width=500,height=500)
 window.title("Typing Test Speed")
-second=0
-def increament_time(second): # add a functionality to call this function for each second , if only  user start typing
-    second+=1
+second =0
+def calcualte_wpm(second): # add a functionality to call this function for each second , if only  user start typing
 
-    if second==60:
-        pass
+    if second>=60:
+        to_min=second/60
+
         #call a function for a fetchiing text data adn caluculatiing wpm
         #to calculate wpa fetch all data then divide by 5 then by total minutes
 
@@ -17,9 +17,12 @@ def increament_time(second): # add a functionality to call this function for eac
 
 
 
-def detect_typing():
-    text_data= text_box.get()
-
+def detect_and_increment(second):
+    text_data= len(text_box.get("1.0", "end-1c"))
+    if text_data>0:
+        calculate_wpm(second)
+        second += 1
+    window.after(1000, lambda:detect_and_increment(second))
 
 def catch_input():
     pass
@@ -47,5 +50,7 @@ text_box = Text(window, wrap="word", height=10, width=40)
 text_box.place(relx=0.5,rely=0.8, anchor=CENTER)
 
 
+
+detect_and_increment(second)
 
 mainloop()
